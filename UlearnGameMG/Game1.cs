c018dfa2bab68747879b0dc7178181525a0a83d7
semakol <kolyvanov.sema@yandex.ru>
2024-04-24 19:41:43 +0500

    Добавьте файлы проекта.

---
+++ UlearnGameMG/Game1.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace UlearnGameMG
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Map Map_map;
+        private Texture2D block;
+        private Vector2 Scale;
+        private Rectangle cellRect = new Rectangle(0,0, 126, 72);
+        
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
+        }
+
+        protected override void Initialize()
+        { 
+            base.Initialize();
+            Map_map = new Map(block);
+            Scale = new Vector2(_graphics.PreferredBackBufferWidth / 1280, _graphics.PreferredBackBufferHeight / 720);
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            block = Content.Load<Texture2D>("isometric_pixel_0014");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            Map_map.Draw(_spriteBatch);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ UlearnGameMG/Map.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+
+namespace UlearnGameMG
+{
+    public class Map
+    {
+        private MapCell[,] mapCells = new MapCell[8, 8];
+
+        public Map(Texture2D texture)
+        {
+            for(int x = 0; x < 8; x++)
+                for(int y = 0; y < 8; y++)
+                {
+                    mapCells[x, y] = new MapCell(new Vector2(x, y), texture);
+                }
+        }
+
+        public void Draw(SpriteBatch _spriteBatch) 
+        {
+            var rift = new Vector2(200, 250);
+            for (int i = 0; i < mapCells.GetLength(0); i++)
+                for (int j = mapCells.GetLength(1) - 1; j >= 0; j--)
+                {
+                    var texture = mapCells[i, j].texture;
+                    _spriteBatch.Draw(
+                        mapCells[i, j].texture,
+                        new Vector2(
+                            (j * texture.Width / 2) + (i * texture.Width / 2) + rift.X, 
+                            (i * texture.Height / 2 * 0.489f) - (j * texture.Height / 2 * 0.489f) + rift.Y),
+                        new Color(255, 255, 255)
+                        );
+                }
+        }
+
+        public Vector2 cordToIso(Vector2 cord)
+        {
+            return new Vector2(cord.X - cord.Y, (cord.X + cord.Y) / 2);
+        }
+    }
+
+    public class MapCell
+    {
+        public Vector2 Location;
+        public Texture2D texture;
+
+        public MapCell(Vector2 location, Texture2D texture) 
+        { 
+            this.Location = location; 
+            this.texture = texture;
+        }
+    }
+
+    
+}
