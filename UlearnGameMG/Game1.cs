6466b6db408a3c319f24765d6e62a4de0ed59055
semakol <kolyvanov.sema@yandex.ru>
2024-04-28 14:52:16 +0500

    v0.1

--- UlearnGameMG/Game1.cs
+++ UlearnGameMG/Game1.cs
@@ -11,9 +11,15 @@ namespace UlearnGameMG
         private SpriteBatch _spriteBatch;
         private Map Map_map;
         private Texture2D block;
-        private Vector2 Scale;
+        private Texture2D mark;
+        private SpriteFont font;
+        Vector2 position = Vector2.Zero;
+        private readonly Vector2 Scale;
         private Rectangle cellRect = new Rectangle(0,0, 126, 72);
+        private MouseState lastMouseState;
+        private readonly Matrix screenXform;
         
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -21,25 +27,39 @@ namespace UlearnGameMG
             IsMouseVisible = true;
             _graphics.PreferredBackBufferWidth = 1280;
             _graphics.PreferredBackBufferHeight = 720;
+            var screenScale = _graphics.PreferredBackBufferHeight / 720.0f;
+            screenXform = Matrix.CreateScale(screenScale, screenScale, 1.0f);
+            Scale = new Vector2(_graphics.PreferredBackBufferWidth / 1280, _graphics.PreferredBackBufferHeight / 720);
         }
 
         protected override void Initialize()
         { 
             base.Initialize();
             Map_map = new Map(block);
-            Scale = new Vector2(_graphics.PreferredBackBufferWidth / 1280, _graphics.PreferredBackBufferHeight / 720);
+            
+            
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             block = Content.Load<Texture2D>("isometric_pixel_0014");
+            mark = Content.Load<Texture2D>("mark");
+            font = Content.Load<SpriteFont>("arial");
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+
+            MouseState currentMouseState = Mouse.GetState();
+
+            if (currentMouseState.X != lastMouseState.X ||
+                currentMouseState.Y != lastMouseState.Y)
+                position = new Vector2(currentMouseState.X, currentMouseState.Y);
+            lastMouseState = currentMouseState;
+            Map_map.CheckMouseCell(position);
 
             base.Update(gameTime);
         }
@@ -48,8 +68,8 @@ namespace UlearnGameMG
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            _spriteBatch.Begin();
-            Map_map.Draw(_spriteBatch);
+            _spriteBatch.Begin(SpriteSortMode.Deferred, null, null, null, null, null, screenXform);
+            Map_map.Draw(_spriteBatch, mark, font);
             _spriteBatch.End();
 
             base.Draw(gameTime);
--- UlearnGameMG/Map.cs
+++ UlearnGameMG/Map.cs
@@ -7,7 +7,11 @@ namespace UlearnGameMG
 {
     public class Map
     {
+        static Vector2 size = new Vector2(126, 75); 
         private MapCell[,] mapCells = new MapCell[8, 8];
+        private Vector2 rift = new Vector2(140, 300);
+        private Vector2 mousePos = new Vector2(0,0);
+        private (int, int) nowCell = (0, 0);
 
         public Map(Texture2D texture)
         {
@@ -18,26 +22,61 @@ namespace UlearnGameMG
                 }
         }
 
-        public void Draw(SpriteBatch _spriteBatch) 
+        public void Draw(SpriteBatch _spriteBatch, Texture2D mark, SpriteFont font) 
         {
-            var rift = new Vector2(200, 250);
-            for (int i = 0; i < mapCells.GetLength(0); i++)
-                for (int j = mapCells.GetLength(1) - 1; j >= 0; j--)
+            int t = 0;
+            for (int j = 0; j < mapCells.GetLength(0); j++)
+                for (int i = mapCells.GetLength(1) - 1; i >= 0; i--)
                 {
-                    var texture = mapCells[i, j].texture;
+                    
+                    var texture = mapCells[j, i].texture;
                     _spriteBatch.Draw(
-                        mapCells[i, j].texture,
+                        texture,
+                        //cordToIso(new Vector2(i * size.X/2, j * size.Y/1.2f)) + rift,
                         new Vector2(
-                            (j * texture.Width / 2) + (i * texture.Width / 2) + rift.X, 
-                            (i * texture.Height / 2 * 0.489f) - (j * texture.Height / 2 * 0.489f) + rift.Y),
-                        new Color(255, 255, 255)
+                            (i * size.X / 2) + (j * size.X / 2) + rift.X, 
+                            (j * size.Y / 2) - (i * size.Y / 2) + rift.Y),
+                        ((i,j) == nowCell ? new Color(255, 191, 0) : new Color(255, 255, 255))
                         );
+                    //_spriteBatch.DrawString(font, j.ToString() + i.ToString(), new Vector2(
+                    //        (i * size.X / 2) + (j * size.X / 2) + rift.X,
+                    //        (j * size.Y / 2 * 0.489f) - (i * size.Y / 2 * 0.489f) + rift.Y), new Color(255, 255, 255));
+                    t++;
                 }
+            for (int x = 0; x < 9; x++)
+                for (int y = 0; y < 9; y++)
+                {
+                    _spriteBatch.Draw(mark, new Vector2(x * 73.316f, y * 73.316f), new Color(255, 255, 255));
+                }
+            _spriteBatch.Draw(mark, mousePos, new Color(255, 255, 255));
         }
 
-        public Vector2 cordToIso(Vector2 cord)
+        public void CheckMouseCell(Vector2 isoPos1)
         {
-            return new Vector2(cord.X - cord.Y, (cord.X + cord.Y) / 2);
+            var cordPos = IsoToCord(isoPos1 - rift - new Vector2(0,size.Y / 2));
+            if (cordPos.X < 0 || cordPos.Y < 0) nowCell = (-1, -1);
+            else nowCell = ((int)(cordPos.X / 73.316f), (int)(cordPos.Y / 73.316f));
+            mousePos = cordPos;
+        }
+
+        private Vector2 cordToIso(Vector2 position)
+        {
+            float sin = 0.5105f;
+            float cos = 0.85985f;
+            return new Vector2(
+                (position.X * cos + position.Y * cos),
+                (-position.X * sin + position.Y * sin)
+                );
+        }
+
+        private Vector2 IsoToCord(Vector2 position)
+        {
+            float a = 0.5814967f;
+            float b = 0.9794319f;
+            return new Vector2(
+                (position.X * a - position.Y * b),
+                (position.X * a + position.Y * b)
+                );
         }
     }
 
@@ -45,6 +84,7 @@ namespace UlearnGameMG
     {
         public Vector2 Location;
         public Texture2D texture;
+        public GameObject
 
         public MapCell(Vector2 location, Texture2D texture) 
         { 
